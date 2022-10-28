7e5206390931c128e0b23371656e9227bd6b5d5b
Wilovy09 <SrWilovy09@gmail.com>
2022-10-27 22:15:43 -0500

    QR maker in python

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import qrcode
 from PIL import Image, ImageDraw
 objeto = input('What will we make a qr code?\n¿A que le haremos un codigo qr?\n>>')
-qr = qrcode.QRCode(version=1, box_size=10, border=5)
+qr = qrcode.QRCode(version=1, box_size=10, border=1)
 qr.add_data(objeto)
 qr.make(fit=True)
 img = qr.make_image(fill='black', back_color="white")
